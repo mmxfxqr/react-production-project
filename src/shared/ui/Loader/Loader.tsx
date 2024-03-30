699834aa7952f98e64780570e47eb6cc2dcb14db
mmxfxqr <holodov.sas@yandex.ru>
2024-03-30 09:35:31 +0300

    add page load

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import "./Loader.scss";
+
+interface LoaderProps {
+  className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+  return (
+    <div className={classNames("lds-ring", {}, [className])}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
